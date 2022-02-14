1c063854091d05bfefae01a923504ab926593ede
Dennis Schmidt <dennis.travbartendermail.com>
2022-02-14 13:20:22 +0100

    feat(map): added overview defibrillators

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../organisms/layout";
 
 const Page = () => {
-
 	return (
 		<Layout>
 			<Head>
@@ -12,6 +11,12 @@ const Page = () => {
 			</Head>
 			<h1>First Aid Hero</h1>
 			<a href="tel:+4911880">Notruf</a>
+			<br></br>
+			<iframe
+				src="https://www.google.com/maps/d/embed?mid=1TrE-jsqko06LoLNIyPgFd4e48Lo&hl=de&ehbc=2E312F"
+				width="375"
+				height="812"
+			></iframe>
 		</Layout>
 	);
 };
