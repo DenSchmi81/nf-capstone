c2e53639e078f604b82adabd9abebf8d642193aa
Dennis Schmidt <dennis.travbartendermail.com>
2022-02-15 14:51:24 +0100

    fix: added type to button

--- src/pages/courseBooking.jsx
+++ src/pages/courseBooking.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Link from "next/link";
 import Layout from "../organisms/layout";
 
 const Page = () => {
@@ -10,6 +11,9 @@ const Page = () => {
 				<meta key="description" name="description" content="First Aid project" />
 			</Head>
 			<h1>First Aid Hero</h1>
+			<button type="button">
+				<Link href="https://www.erstehilfe.de/startseite">Buche hier Deinen Kurs!</Link>
+			</button>
 			<a href="tel:+4911880">Notruf</a>
 		</Layout>
 	);
