cfb838a5cea40665a8f7b75edfac210ad8b7e83a
DenSchmi81 <94965417+DenSchmi81@users.noreply.github.com>
2022-02-15 13:44:38 +0100

    Feat/pages (#4)
    * feat: added pages and buttons
    
    * feat: added pages and buttons
    
    * fix: erased unused Link component
    
    Co-authored-by: Dennis Schmidt <dennis.travbartendermail.com>

--- src/pages/defibrillators.jsx
+++ src/pages/defibrillators.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
 import Layout from "../organisms/layout";
 
@@ -11,9 +10,6 @@ const Page = () => {
 				<meta key="description" name="description" content="First Aid project" />
 			</Head>
 			<h1>First Aid Hero</h1>
-			<button type="button">
-				<Link href="/overview">Rette Leben!</Link>
-			</button>
 			<a href="tel:+4911880">Notruf</a>
 		</Layout>
 	);
