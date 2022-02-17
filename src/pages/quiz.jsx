ede5c63e2005204647f1cd998b2f9a941836b8f6
DenSchmi81 <94965417+DenSchmi81@users.noreply.github.com>
2022-02-17 10:16:23 +0100

    Feat/question component (#13)
    * feat: added button component
    
    * fix: fixed missing classname for button
    
    * fix: removed unused import
    
    Co-authored-by: Dennis Schmidt <dennis.travbartendermail.com>

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "../organisms/layout";
+/*
 import StartQuiz from "../atoms/button/StartQuiz.js";
+*/
+import Question from "../molecules/Question.js";
+import StartQuiz from "../atoms/button/StartQuiz.js";
+
 /*import "bulma/css/bulma.min.css";*/
 
 const Page = () => {
@@ -11,6 +16,8 @@ const Page = () => {
 				<title key="title">Erste-Hilfe-Quiz</title>
 				<meta key="description" name="description" content="First Aid project" />
 			</Head>
+			{/* <StartQuiz /> */}
+			<Question />
 			<StartQuiz />
 			<a href="tel:+4911880">Notruf</a>
 		</Layout>
