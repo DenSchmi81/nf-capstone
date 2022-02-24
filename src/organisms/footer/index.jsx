69692a59250b2bc98ebf7abbe81846dc14aad60c
Dennis Schmidt <dennis.travbartendermail.com>
2022-02-24 15:08:08 +0100

    feat: implement theme toggle logic

--- src/organisms/footer/index.jsx
+++ src/organisms/footer/index.jsx
@@ -8,9 +8,9 @@ import Brightness4Icon from "@mui/icons-material/Brightness4";
 import { styled } from "@mui/material/styles";
 import Fab from "@mui/material/Fab";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import emergencyCall from "../../ions/icons/emergencyCall.png";
 import Image from "next/image";
+import useColorMode from "/src/ions/hooks/store/useColorMode";
 
 const StyledFab = styled(Fab)({
 	position: "absolute",
@@ -23,31 +23,39 @@ const StyledFab = styled(Fab)({
 
 const Footer = () => {
 	const [value, setValue] = React.useState("recents");
+	const colorMode = useColorMode(state => state.colorMode);
+	const toggleColorMode = useColorMode(state => state.toggleColorMode);
 
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
+	console.log(colorMode);
 
 	return (
 		<footer>
-			<Box sx={{ width: 500 }}>
-				<Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
-					<BottomNavigation value={value} onChange={handleChange}>
-						<BottomNavigationAction icon={<HomeIcon />} href="../" />
-						<BottomNavigationAction icon={<HealingIcon />} href="../bookmark" />
-						<BottomNavigationAction icon={<Brightness4Icon />} />
-						<StyledFab
-							aria-label="add"
-							size="medium"
-							href="tel:+4911880"
-							sx={{ background: "#F25764" }}
-						>
-							<Image src={emergencyCall} />
-						</StyledFab>
-						<BottomNavigationAction icon={<RestartAltIcon />} href="../quiz" />
-					</BottomNavigation>
-				</Paper>
-			</Box>
+			<BottomNavigation sx={{ mt: 5, visibility: "hidden" }} />
+			<Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
+				<BottomNavigation value={value} onChange={handleChange}>
+					<BottomNavigationAction icon={<HomeIcon />} href="../" />
+					<BottomNavigationAction icon={<HealingIcon />} href="../bookmark" />
+					<BottomNavigationAction
+						button
+						icon={<Brightness4Icon />}
+						onClick={() => {
+							toggleColorMode();
+						}}
+					/>
+					<StyledFab
+						aria-label="add"
+						size="medium"
+						href="tel:+4911880"
+						sx={{ background: "#F25764" }}
+					>
+						<Image src={emergencyCall} />
+					</StyledFab>
+					<BottomNavigationAction icon={<RestartAltIcon />} href="../quiz" />
+				</BottomNavigation>
+			</Paper>
 		</footer>
 	);
 };
