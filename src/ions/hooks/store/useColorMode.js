b15ea360ddb7d24baa13569d6ea5c45ee1f2a709
Dennis Schmidt <dennis.travbartendermail.com>
2022-02-25 01:53:10 +0100

    fix: removed doubled code

--- src/ions/hooks/store/useColorMode.js
+++ src/ions/hooks/store/useColorMode.js
@@ -2,7 +2,6 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 
 const useColorMode = create(
-
 	persist(
 		(set, get) => ({
 			colorMode: true,
@@ -13,16 +12,6 @@ const useColorMode = create(
 		}),
 		{ name: "firstAidColorMode" }
 	)
-
-	persist((set, get) => ({
-		colorMode: true,
-		toggleColorMode: () => {
-			const colorMode = get().colorMode;
-			set({ colorMode: !colorMode });
-		},
-	})),
-	{ name: "firstAidColorMode" }
-
 );
 
 export default useColorMode;
